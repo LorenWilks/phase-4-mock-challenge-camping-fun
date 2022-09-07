154267713e84164dcbc66cd37fe7a10d5525107a
Loren Wilks <Loren.wilks@gmail.com>
2022-09-07 17:39:56 -0600

    commit tree f8a6893b1dcbf67a828c175623b3ddf7aed545b3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,17 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+rescue_from ActiveRecord::RecordInvalid, with: :render_invalid
+
+
+private
+
+def render_not_found(err)
+  render json: {error: "#{err.model} not found"}, status: 404
+end
+
+def render_invalid(err)
+  render json: {errors: err.record.errors.full_messages}, status: 422
+end
 
 end
